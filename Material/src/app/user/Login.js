f82457239a3c8cd21204a1c721ef0f789d9b7078
gristar <sheng.fighting@qq.com>
2017-07-27 18:27:16 +0800

    fix login page

--- Material/src/app/user/Login.js
+++ Material/src/app/user/Login.js
@@ -1,12 +1,31 @@
-/**
- * Created by gristar on 2017/7/25.
- */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Login extends Component {
-    render() {
-        return (
-            <div>login page</div>
+    loginAct() {
+        alert('hhh');
+    }
+    render(){
+        return(
+            <MuiThemeProvider>
+                <div>
+                    <TextField
+                        name="username"
+                        hintText="手机或邮箱"
+                    />
+                    <TextField
+                        name="password"
+                        hintText="密码"
+                    />
+                    <RaisedButton
+                        label="登录"
+                        primary={true}
+                        onTouchTap={this.loginAct}
+                    />
+                </div>
+            </MuiThemeProvider>
         )
     }
 }
