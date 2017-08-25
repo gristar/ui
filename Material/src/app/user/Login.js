f45cdd8cee265d2221bcbc5fb3be29698bcfd6e9
gristar <sheng.fighting@qq.com>
2017-08-25 18:12:30 +0800

    add template

--- Material/src/app/user/Login.js
+++ Material/src/app/user/Login.js
@@ -1,31 +1,54 @@
 import React, {Component} from 'react';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Link} from 'react-router';
 
-class Login extends Component {
-    loginAct() {
-        alert('hhh');
+const styles = {
+    wrap: {
+        margin: 'auto',
+        padding: '2rem 0',
+        width: '256px'
+    },
+    groupInputs: {
+        padding: '1px 0',
+        border: '1px solid #d5d5d5',
+        borderRadius: '3px'
+    },
+    input: {
+        padding: '1em .8em',
+        width: '100%',
+        boxSizing: 'border-box'
+    },
+    inputLogin: {
+        width: '100%'
     }
+};
+const loginAct = () => {
+    alert('hhh');
+};
+class TextField extends Component {
+    render(){
+        return (
+            <input></input>
+        )
+    }
+}
+class Login extends Component {
     render(){
         return(
-            <MuiThemeProvider>
-                <div>
+            <div style={styles.wrap}>
+                <Link to="/register">注册</Link>
+                <div style={styles.groupInputs}>
                     <TextField
                         name="username"
+                        style={styles.input}
                         hintText="手机或邮箱"
                     />
                     <TextField
                         name="password"
+                        style={styles.input}
                         hintText="密码"
                     />
-                    <RaisedButton
-                        label="登录"
-                        primary={true}
-                        onTouchTap={this.loginAct}
-                    />
                 </div>
-            </MuiThemeProvider>
+            </div>
         )
     }
 }
